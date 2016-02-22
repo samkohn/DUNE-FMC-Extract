3456700bef2af62c3a8f548109c409c21879ce51
Sam <kohn@berkeley.edu>
2016-02-22 17:44:48 -0600

    Update beam flux fetcher to correct source
    Previously drew from FastMC output files as a temporary placeholder.
    Now it correctly reads in the beam flux file provided to the FastMC as
    an input, taken from the flux group's simulations.

--- ExtractBeamFluxes.C
+++ ExtractBeamFluxes.C
@@ -1,52 +1,56 @@
 #include "Configuration.C"
-
+/*
+ * This macro extracts the beam flux from the input to the Fast Monte
+ * Carlo. The flux is provided from 0 GeV to approximately 100 GeV in
+ * increments of 125 MeV. When the energy bins requested do not line up
+ * with 125 MeV increments, a linear interpolation is performed.
+ */
 int ExtractBeamFluxes(const int NBINS, const double EMIN,
         const double EMAX)
 {
-    std::vector<std::string> filenames;
-    filenames.push_back("nuflux_numuflux_numu");
-    filenames.push_back("nuflux_nueflux_nue");
-    filenames.push_back("nuflux_numubarflux_numubar");
-    filenames.push_back("nuflux_nuebarflux_nuebar");
-    filenames.push_back("anuflux_numuflux_numu");
-    filenames.push_back("anuflux_nueflux_nue");
-    filenames.push_back("anuflux_numubarflux_numubar");
-    filenames.push_back("anuflux_nuebarflux_nuebar");
+
     char outputdir[100];
-    std::string prefix = std::string(CFG_OutputDirectory(outputdir)) +
-        "oscprob/";
-    std::string suffix = "__OSCPROB.root";
-    std::vector<std::string>::iterator it = filenames.begin();
-    for(it; it != filenames.end(); ++it)
+    std::string filename = std::string(CFG_OutputDirectory(outputdir)) +
+        "fmcflux/g4lbne_v3r2p4b_FHC_FD_RIK.root";
+    TFile* fin = TFile::Open(filename.c_str(), "READ");
+    if(!fin)
+    {
+        std::cout << "ERROR: ROOT file does not exist.\n";
+        return 1;
+    }
+    std::vector<std::string> histogramnames;
+    histogramnames.push_back("numu_flux");
+    histogramnames.push_back("nue_flux");
+    histogramnames.push_back("numubar_flux");
+    histogramnames.push_back("nuebar_flux");
+    histogramnames.push_back("nutau_flux");
+    histogramnames.push_back("nutaubar_flux");
+    std::vector<std::string>::iterator it = histogramnames.begin();
+    for(it; it != histogramnames.end(); ++it)
     {
         // Create the correct file name
-        std::string temp = *it;
-        it->assign(prefix);
-        it->append(temp);
-        it->append(suffix);
-        std::cout << *it << std::endl;
-        std::string& filename = *it;
+        std::string& histname = *it;
+        std::cout << histname << std::endl;
 
-        // Open the file
-        TFile* fin = TFile::Open(filename.c_str(), "READ");
-        if(!fin)
-        {
-            std::cout << "ERROR: ROOT file does not exist.\n";
-            return 1;
-        }
-        TTree* fluxData = (TTree*) fin->Get("OSCPROB");
         TCanvas* c1 = new TCanvas();
-        TH1D* spectrum = new TH1D(temp.c_str(), temp.c_str(),
-                NBINS, EMIN, EMAX);
-        // Draw the energy spectrum of the unoscillated beam neutrinos
-        fluxData->Draw((std::string("Ev>>") + temp).c_str());
-        spectrum->GetXaxis()->SetTitle("E_{#nu} [GeV]");
-        spectrum->GetYaxis()->SetTitle("Spectrum [a.u.]");
-
+        TH1D* spectrum = (TH1D*) (fin->Get(histname.c_str()));
+        // TODO dump the histogram contents into a TGraph for
+        // interpolation.
+        // The flux is given in units of neutrinos/GeV/m^2/POT
+        // Expect histogram to have <500 bins
+        const size_t INPUT_HIST_BINS = 500;
+        double spectrumx[INPUT_HIST_BINS];
+        double spectrumy[INPUT_HIST_BINS];
+        for(int i = 1; i <= spectrum->GetNbinsX(); ++i)
+        {
+            spectrumx[i-1] = spectrum->GetBinLowEdge(i);
+            spectrumy[i-1] = spectrum->GetBinContent(i);
+        }
+        TGraph* spectrum_gr = new TGraph(spectrum->GetNbinsX(), spectrumx, spectrumy);
         // Dump the results to a CSV file
         std::ofstream outputfile;
         outputfile.open((std::string("/afs/fnal.gov/files/home/room3/") + 
-                    "skohn/outputs/beam-flux/" + temp +
+                    "skohn/outputs/beam-flux2/" + histname +
                     Form("%d.csv", NBINS)).c_str());
         if(!outputfile.is_open())
         {
@@ -57,13 +61,16 @@ int ExtractBeamFluxes(const int NBINS, const double EMIN,
         {
             std::cout << "INFO: Opened output file\n";
         }
-        for(size_t ebin = 1; ebin <= NBINS; ++ebin)
+        const double ESTEP = (EMAX - EMIN)/NBINS;
+        for(size_t ebin = 0; ebin < NBINS; ++ebin)
         {
-            double value = spectrum->GetBinContent(ebin);
+            double energy = EMIN + ESTEP * ebin;
+            double value = spectrum_gr->Eval(energy);
             outputfile << value << "\n";
         }
         outputfile.close();
-        fin->Close();
+        delete spectrum_gr;
     }
+    fin->Close();
     return 0;
 }
